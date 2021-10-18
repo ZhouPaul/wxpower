f6d80033e9d9a85274ad2f4b6a1dace7981b5c99
xh9999 <987863597@qq.com>
2021-10-18 14:10:27 +0800

    第二次上传

--- route/admin.js
+++ route/admin.js
@@ -3,4 +3,19 @@ const admin=express.Router();
 admin.get('/index',(req,res)=>{ 
     res.render('culture.html');
 });
+admin.get('/intro',(req,res)=>{
+    res.send('intro');
+});
+admin.get('/honor',(req,res)=>{
+    res.send('honor');
+});
+admin.get('/policy',(req,res)=>{
+    res.send('policy');
+});
+admin.get('/quality',(req,res)=>{
+    res.send('quality');
+});
+admin.get('/culture',(req,res)=>{
+    res.send('culture')
+})
 module.exports=admin;
