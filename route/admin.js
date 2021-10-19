8b80a400553407958303e2d15e02f8c3e9909023
xh9999 <987863597@qq.com>
2021-10-19 15:58:23 +0800

    '下午提交'

--- route/admin.js
+++ route/admin.js
@@ -1,18 +1,25 @@
-const express=require('express');
-const admin=express.Router();
-admin.get('/intro',(req,res)=>{
-    res.render('intro.html');
+const express = require('express');
+const Develop = require('../models/power');
+const admin = express.Router();
+admin.get('/intro', async(req, res) => {
+    var id = req.query.id;
+    if (id) {
+        const content=await Develop.findOne({"num":id});
+        res.render('detail.html',{content:content});
+    }else {
+        res.render('intro.html');
+    }
 });
-admin.get('/honor',(req,res)=>{
+admin.get('/honor', (req, res) => {
     res.render('honor.html');
 });
-admin.get('/policy',(req,res)=>{
+admin.get('/policy', (req, res) => {
     res.render('policy.html');
 });
-admin.get('/quality',(req,res)=>{
+admin.get('/quality', (req, res) => {
     res.render('quality.html');
 });
-admin.get('/culture',(req,res)=>{
+admin.get('/culture', (req, res) => {
     res.render('culture.html');
 })
-module.exports=admin;
+module.exports = admin;
