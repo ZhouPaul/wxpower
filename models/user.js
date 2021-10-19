9f70a5742562659350f9cbd2228cc22c11298c9b
xuekai1998 <1727754343@qq.com>
2021-10-19 20:17:41 +0800

    '第五次修改(数据完成)'

--- models/user.js
+++ models/user.js
@@ -7,9 +7,28 @@ var listScheme = mongoose.Schema({
     KW:Number,
     oil:Number,
     temperature:String,
+    travel:String,
+    displacement:Number,
+    normKW:Number,
+    enOil:String,
+    fuOil:Number,
+    lube:Number,
+    refrigerant:Number,
+    suttle:Number,
 })
 // {"model":"WD145E3TAD35","speed":"1500","KW":"353","oil":"198","temperature":"<700"}
+// {{"model":"WD129TAD23","travel":"135×150","displacement":"12.9","speed":"1500","normKW":"235","KW":"259","enOil":"≤0.67","fuOil":"198","temperature":"≤650","lube":"40","refrigerant":"55","suttle":"1350"}}
 
 var User = mongoose.model("list_1a1",listScheme);
+var User2 = mongoose.model("list_1a2",listScheme);
+var User3 = mongoose.model("list_2a1",listScheme);
+var User4 = mongoose.model("list_2b1",listScheme);
+var User5 = mongoose.model("list_2c1",listScheme);
+var User6 = mongoose.model("list_2d1",listScheme);
+var User7 = mongoose.model("list_3a1",listScheme);
+var User8 = mongoose.model("list_3a2",listScheme);
+var User9 = mongoose.model("list_3c1",listScheme);
+var User10 = mongoose.model("list_3c2",listScheme);
+var User11 = mongoose.model("list_3c3",listScheme);
 
-module.exports = User;
+module.exports = {User,User2,User3,User4,User5,User6,User7,User8,User9,User10,User11};
--- route/admin.js
+++ route/admin.js
@@ -1,25 +1,87 @@
 const express = require("express");
 const route = express.Router();
-const User = require("../models/user")
+const {User,User2,User3,User4,User5,User6,User7,User8,User9,User10,User11} = require("../models/user")
+// const User2 = require("../models/user")
 //产品展示首页接口
-route.get("/product1",(req,res)=>{
-    var user = req.session.user;
-    res.render("product1.html",{user})
+route.get("/product1", (req, res) => {
+    res.render("product1.html")
 });
-route.get("/product2",(req,res)=>{
-    var user = req.session.user;
-    res.render("product2.html",{user})
+route.get("/product2", (req, res) => {
+    res.render("product2.html")
 });
-route.get("/product3",(req,res)=>{
-    var user = req.session.user;
-    res.render("product3.html",{user})
+route.get("/product3", (req, res) => {
+    res.render("product3.html")
 });
 //产品详情页面接口
-route.get("/detail",(req,res)=>{
-    User.find(function(err,datas){
-        console.log(datas,"xxxxxxx")
+route.get("/detail", async (req, res, next) => {
+    const data1 = await User.find();
+    const data2 = await User2.find();
+    // console.log(users2);
+    res.render("../views/detail/detail.html", {
+        listMsg1: data1,
+        listMsg2: data2,
     })
-    res.render("../views/detail/detail.html")
 })
+route.get("/detail2", async (req, res, next) => {
+    const data3 = await User3.find();
+    // console.log(users2);
+    res.render("../views/detail/detail2.html", {
+        listMsg3: data3,
+
+    })
+})
+route.get("/detail3", async (req, res, next) => {
+    const data4 = await User4.find();
+    // console.log(users2);
+    res.render("../views/detail/detail3.html", {
+        listMsg4: data4,
+    })
+})
+route.get("/detail4", async (req, res, next) => {
+    const data5 = await User5.find();
+    // console.log(users2);
+    res.render("../views/detail/detail4.html", {
+        listMsg5: data5,
+    })
+})
+route.get("/detail5", async (req, res, next) => {
+    const data6 = await User6.find();
+    // console.log(users2);
+    res.render("../views/detail/detail5.html", {
+        listMsg6: data6,
+    })
+})
+route.get("/detail6", async (req, res, next) => {
+    const data7 = await User7.find();
+    const data8 = await User8.find();
+    // console.log(users2);
+    res.render("../views/detail/detail6.html", {
+        listMsg7: data7,
+        listMsg8: data8,
+    })
+})
+route.get("/detail7", async (req, res, next) => {
+    const data7 = await User7.find();
+    const data8 = await User8.find();
+    // console.log(users2);
+    res.render("../views/detail/detail7.html", {
+        listMsg7: data7,
+        listMsg8: data8,
+    })
+})
+route.get("/detail8", async (req, res, next) => {
+    const data9 = await User9.find();
+    const data10 = await User10.find();
+    const data11 = await User11.find();
+    // console.log(users2);
+    res.render("../views/detail/detail8.html", {
+        listMsg9: data9,
+        listMsg10: data10,
+        listMsg11: data11,
+    })
+})
+
+
+
 
 module.exports = route;
