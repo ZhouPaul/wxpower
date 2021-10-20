49ffc6deb2b02866e24e3d1072dc38dde236d8d0
xh9999 <987863597@qq.com>
2021-10-20 14:55:16 +0800

    'eee'

--- route/admin.js
+++ route/admin.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const route = express.Router();
+const admin = express.Router();
 const {User,User2,User3,User4,User5,User6,User7,User8,User9,User10,User11} = require("../models/user");
 
 //产品展示首页接口
-route.get("/product", (req, res) => {
+admin.get("/product", (req, res) => {
     res.render("product.html")
 });
 
-route.get("/product1", (req, res) => {
+admin.get("/product1", (req, res) => {
     res.render("product1.html")
 });
-route.get("/product2", (req, res) => {
+admin.get("/product2", (req, res) => {
     res.render("product2.html")
 });
-route.get("/product3", (req, res) => {
+admin.get("/product3", (req, res) => {
     res.render("product3.html")
 });
 //产品详情页面接口
-route.get("/detail", async (req, res, next) => {
+admin.get("/detail", async (req, res, next) => {
     const data1 = await User.find();
     const data2 = await User2.find();
     // console.log(users2);
@@ -26,7 +26,7 @@ route.get("/detail", async (req, res, next) => {
         listMsg2: data2,
     })
 })
-route.get("/detail2", async (req, res, next) => {
+admin.get("/detail2", async (req, res, next) => {
     const data3 = await User3.find();
     // console.log(users2);
     res.render("../views/detail/detail2.html", {
@@ -34,28 +34,28 @@ route.get("/detail2", async (req, res, next) => {
 
     })
 })
-route.get("/detail3", async (req, res, next) => {
+admin.get("/detail3", async (req, res, next) => {
     const data4 = await User4.find();
     // console.log(users2);
     res.render("../views/detail/detail3.html", {
         listMsg4: data4,
     })
 })
-route.get("/detail4", async (req, res, next) => {
+admin.get("/detail4", async (req, res, next) => {
     const data5 = await User5.find();
     // console.log(users2);
     res.render("../views/detail/detail4.html", {
         listMsg5: data5,
     })
 })
-route.get("/detail5", async (req, res, next) => {
+admin.get("/detail5", async (req, res, next) => {
     const data6 = await User6.find();
     // console.log(users2);
     res.render("../views/detail/detail5.html", {
         listMsg6: data6,
     })
 })
-route.get("/detail6", async (req, res, next) => {
+admin.get("/detail6", async (req, res, next) => {
     const data7 = await User7.find();
     const data8 = await User8.find();
     // console.log(users2);
@@ -64,7 +64,7 @@ route.get("/detail6", async (req, res, next) => {
         listMsg8: data8,
     })
 })
-route.get("/detail7", async (req, res, next) => {
+admin.get("/detail7", async (req, res, next) => {
     const data7 = await User7.find();
     const data8 = await User8.find();
     // console.log(users2);
@@ -73,7 +73,7 @@ route.get("/detail7", async (req, res, next) => {
         listMsg8: data8,
     })
 })
-route.get("/detail8", async (req, res, next) => {
+admin.get("/detail8", async (req, res, next) => {
     const data9 = await User9.find();
     const data10 = await User10.find();
     const data11 = await User11.find();
@@ -85,4 +85,4 @@ route.get("/detail8", async (req, res, next) => {
     })
 })
 
-module.exports = route;
+module.exports = admin;
